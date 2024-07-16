61adecc1e201a81e1f7b70f151b2df2e11d58420
Luiz Vulpi <luizvulpi@hotmail.com>
2024-07-16 16:23:27 -0300

    meu primeiro commit

---
+++ main.py
@@ -0,0 +1,18 @@
+print("jornada de dados")
+
+# variaveis
+idade = 36
+nome = "Luiz"
+
+# print
+print(idade)
+
+# Soma
+nova_idade = idade + 1
+print(nova_idade)
+
+# lista
+lista = [1,2,3,4,5,]
+
+# método
+lista_invertida = lista.reverse()
